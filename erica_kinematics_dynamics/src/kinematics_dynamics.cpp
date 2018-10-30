c16d9a4f91dfccca29b2df81043d79e0669f66ae
Jay-Song <sguswhd21@gmail.com>
2018-10-30 20:31:53 +0900

    add files for erica_kin_dyn

---
+++ erica_kinematics_dynamics/src/kinematics_dynamics.cpp
@@ -0,0 +1,10 @@
+/*
+ * erica_kinematics_dynamics.cpp
+ *
+ *  Created on: Oct 30, 2018
+ *      Author: jay
+ */
+
+
+
+
---
+++ erica_kinematics_dynamics/src/link_data.cpp
@@ -0,0 +1,10 @@
+/*
+ * link_data.cpp
+ *
+ *  Created on: Oct 30, 2018
+ *      Author: jay
+ */
+
+
+
+
